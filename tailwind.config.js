08389f57a26c896417779cb0cc37fd00dfde49ea
Ernest079 <ernestomendoza.martinez73@gmail.com>
2025-07-23 18:02:02 -0600

    Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-// tailwind.config.js
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx}",
   ],
   theme: {
     extend: {
